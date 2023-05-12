ad68175ca109e8384969bbbe05c1f78d480c2f53
Maitri Bhavsar <maitri.bhavsar@devitpl.com>
2023-05-12 18:57:14 +0530

    Added map

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  useWebMap,
+  useFeatureTable,
+  useMap,
+  useGraphics,
+} from "esri-loader-hooks";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const options = {
+    view: {
+      center: [15, 65],
+      zoom: 4,
+    },
+  };
+
+  const [ref] = useWebMap("e691172598f04ea8881cd2a4adaa45ba", options);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React-arcgis</h1>
+      <div style={{ height: 400 }} ref={ref} />
+      {/* <div style={{ height: 400 }} ref={reference} /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import { Scene, Map } from "@esri/react-arcgis";
+// import { WebMap, WebScene } from "@esri/react-arcgis";
+// import { useWebMap } from "esri-loader-hooks";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    {/* <div style={{ width: "100vw", height: "100vh" }}>
+      <WebMap id="6627e1dd5f594160ac60f9dfc411673f" />
+      <WebScene id="f8aa0c25485a40a1ada1e4b600522681" />
+    </div> */}
+    {/* <Map class="full-screen-map" mapProperties={{ basemap: "satellite" }} /> */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
+);
+
+// ReactDOM.render(<Map />, document.getElementById("container"));
